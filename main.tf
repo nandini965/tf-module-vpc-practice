4d5c509476b2b4eb89edc7152724abfa9cc8829a
nandini965 <nandhini965@gmail.com>
2024-12-13 23:08:38 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@
   }
 }
 
- resource "aws_vpc_peering_connection" "peering_connection" {
-   peer_vpc_id   = var.vpc_id
+ resource "aws_vpc_peering_connection" "peer" {
+   peer_vpc_id   = var.default_vpc_id
    vpc_id        = aws_vpc.main.id
    auto_accept   = true
 
--- vars.tf
+++ vars.tf
@@ -1,2 +1,3 @@
 variable "vpc_id" {}
 variable "cidr_block" {}
+variable "default_vpc_id" {}
