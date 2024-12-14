e5fe557e391ff4f840762f18b3023ba00a6b13ed
nandini965 <nandhini965@gmail.com>
2024-12-14 15:34:56 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@
    enable_dns_hostnames = true
    enable_dns_support = true
  }
+ resource "aws_vpc_peering_connection" "peer" {
+   peer_vpc_id = aws_default_vpc_id
+   vpc_id      = var.vpc_id
+   auto_accept = true
 
-
+ }
 
