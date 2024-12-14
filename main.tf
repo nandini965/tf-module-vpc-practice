d986760c26333a2c523c34eb51cd2e36870f4b7e
nandini965 <nandhini965@gmail.com>
2024-12-14 16:29:41 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -3,9 +3,11 @@
    enable_dns_hostnames = true
    enable_dns_support   = true
  }
- resource "aws_subnet" "main" {
-   vpc_id     = var.vpc_id
-   cidr_block = length(cidr_block)
+
+ module subnets  {
+   source = "./subnets"
+   vpc_id = var.vpc_id
+   cidr_block = var.cidr_block
  }
  resource "aws_vpc_peering_connection" "peer" {
    peer_vpc_id = aws_default_vpc_id
--- vars.tf
+++ vars.tf
@@ -2,3 +2,4 @@ variable "default_vpc_id" {}
 variable "cidr_block" {}
 variable "vpc_id" {}
 variable "default_vpc_cidr" {}
+variable "subnets" {}
